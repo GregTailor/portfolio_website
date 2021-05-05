24a695ea85e4204fa4fa155ed8fe4574db5a1ecc
GregTailor <szg0219@gmail.com>
2021-05-05 08:47:07 +0200

    added fade animation

---
+++ fade.js
@@ -0,0 +1,13 @@
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
+$(window).scroll( function(){
+    $('.hideme').each( function(i){
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if( bottom_of_window >= bottom_of_object ){
+            $(this).animate({'opacity':'1'},1000);
+        }
+    });
+});
